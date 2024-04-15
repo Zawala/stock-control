dd5bc7a31e53fb06267d6d4b2c1ce1d0313b828b
Kelvin zawala <87924723+Zawala@users.noreply.github.com>
2024-04-15 14:08:20 +0200

    protected apis using sactum and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -15,8 +16,14 @@ use App\Http\Controllers\ProductController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::apiResource('products', ProductController::class);
-Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->apiResource('products', ProductController::class);
+Route::middleware('auth:sanctum')->delete('/products/{product}', [ProductController::class, 'destroy']);
+Route::middleware('auth:sanctum')->get('/products', [ProductController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
